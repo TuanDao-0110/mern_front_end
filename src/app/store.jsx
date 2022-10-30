479baaa65d7995d747c14761db5eaa8a0c269f1e
tuan <rickytuan1991@gmail.com>
2022-10-30 16:19:59 +0200

    middle ware

---
+++ src/app/store.jsx
@@ -0,0 +1,11 @@
+import {configureStore} from '@reduxjs/toolkit' 
+import { apiSlice } from './api/apiSlice'
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]:apiSlice.reducer
+  },
+//   this default middleware:
+  middleware: getDefaultMiddleware => 
+   getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
