301498978e88bdc85c543bd3f5803b7d7ef94ebc
tuan <rickytuan1991@gmail.com>
2022-11-13 00:53:53 +0200

    add note

--- src/features/notes/EditNote.jsx
+++ src/features/notes/EditNote.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectnoteById } from './noteApiSlicer'
-import { selectAllUsers } from '../users/usersApiSlice'
-import EditNoteForm from './EditNoteForm'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectnoteById } from "./noteApiSlicer";
+import { selectAllUsers } from "../users/usersApiSlice";
+import EditNoteForm from "./EditNoteForm";
+
 export default function EditNote() {
-  
-  return (
-    <div></div>
-  )
+  const { id } = useParams();
+  const note = useSelector((state) => selectnoteById(state, id));
+  const users = useSelector(selectAllUsers);
+  const content = note && users ? <EditNoteForm note={note} users={users}></EditNoteForm> : <p>Loading...</p>;
+  return content;
 }
